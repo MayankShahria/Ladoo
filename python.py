665b1562820c4441dfcade6059c1f0c93487ab22
MayankShahria <45873094+MayankShahria@users.noreply.github.com>
2020-03-02 20:22:31 +0530

    python.py
    Image classifier code for ladoo

---
+++ python.py
@@ -0,0 +1,4 @@
+# Here we are creatining an image classifier for ladoo
+#First of all we need to collect a lot of images for the ladoo dataset, for that we need to download different images of ladoos from  google 
+#here i have kept 137 images for ladoos for training the data purpose
+#make sure that all the data is there in a classified folder only meant for that purpose only Nou
